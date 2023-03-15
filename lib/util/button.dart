2d1f10daca57a733c4d8142bd44fdb2925a3ccb9
yaseensha <shayaseen51@gmail.com>
2023-03-15 11:30:57 +0530

    coustem button

---
+++ lib/util/button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../core/padding.dart';
+
+class Button extends StatelessWidget {
+  const Button(
+      {Key? key,
+      required this.colorB,
+      required this.colorF,
+      required this.title,
+      required this.style})
+      : super(key: key);
+  final Color colorF;
+  final Color colorB;
+  final String title;
+  final TextStyle style;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: p10,
+      child: SizedBox(
+        height: 50,
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            foregroundColor: colorF,
+            backgroundColor: colorB,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
+            elevation: 10.0,
+          ),
+          child: Padding(
+            padding: p10,
+            child: Text(title, style: style),
+          ),
+        ),
+      ),
+    );
+  }
+}
