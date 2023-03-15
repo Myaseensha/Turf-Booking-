6a0dfa946bc37c431f72380c41ecc41b0da45dbf
yaseensha <shayaseen51@gmail.com>
2023-03-15 11:31:29 +0530

    Coustem Padding

---
+++ lib/core/padding.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+EdgeInsetsGeometry p10 = const EdgeInsets.all(10);
+EdgeInsetsGeometry p20 = const EdgeInsets.all(20);
+EdgeInsetsGeometry p30 = const EdgeInsets.all(30);
+EdgeInsetsGeometry p40 = const EdgeInsets.all(40);
+//---------------------------------------------------
+EdgeInsetsGeometry pL10 = const EdgeInsets.only(left: 10);
+EdgeInsetsGeometry pL20 = const EdgeInsets.only(left: 20);
+EdgeInsetsGeometry pL30 = const EdgeInsets.only(left: 30);
+EdgeInsetsGeometry pL40 = const EdgeInsets.only(left: 40);
+//---------------------------------------------------
+EdgeInsetsGeometry pR40 = const EdgeInsets.only(right: 40);
+EdgeInsetsGeometry pR30 = const EdgeInsets.only(right: 30);
+EdgeInsetsGeometry pR20 = const EdgeInsets.only(right: 20);
+EdgeInsetsGeometry pR10 = const EdgeInsets.only(right: 10);
+//---------------------------------------------------
+EdgeInsetsGeometry pRL10 = const EdgeInsets.only(right: 10, left: 10);
+EdgeInsetsGeometry pRL20 = const EdgeInsets.only(right: 20, left: 20);
+EdgeInsetsGeometry pRL30 = const EdgeInsets.only(right: 30, left: 30);
+EdgeInsetsGeometry pRL40 = const EdgeInsets.only(right: 40, left: 40);
+//------------------------------------------------------
+EdgeInsetsGeometry pBT10 = const EdgeInsets.only(bottom: 10, top: 10);
+EdgeInsetsGeometry pBT20 = const EdgeInsets.only(bottom: 20, top: 20);
+EdgeInsetsGeometry pBT30 = const EdgeInsets.only(bottom: 30, top: 30);
+EdgeInsetsGeometry pBT40 = const EdgeInsets.only(bottom: 40, top: 40);
+//------------------------------------------------------
+EdgeInsetsGeometry pBRL10 =
+    const EdgeInsets.only(bottom: 10, right: 10, left: 10);
+EdgeInsetsGeometry pBRL20 =
+    const EdgeInsets.only(bottom: 20, right: 20, left: 20);
+EdgeInsetsGeometry pBRL30 =
+    const EdgeInsets.only(bottom: 30, right: 30, left: 30);
+EdgeInsetsGeometry pBRL40 =
+    const EdgeInsets.only(bottom: 40, right: 40, left: 40);
+//------------------------------------------------------
+EdgeInsetsGeometry pTRL10 = const EdgeInsets.only(top: 10, right: 10, left: 10);
+EdgeInsetsGeometry pTRL20 = const EdgeInsets.only(top: 20, right: 20, left: 20);
+EdgeInsetsGeometry pTRL30 = const EdgeInsets.only(top: 30, right: 30, left: 30);
+EdgeInsetsGeometry pTRL40 = const EdgeInsets.only(top: 40, right: 40, left: 40);
