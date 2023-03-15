3b1baf52c70fbc0a7aff65163b93dcaa5efa9abd
yaseensha <shayaseen51@gmail.com>
2023-03-15 11:33:06 +0530

    Coustem color file

---
+++ lib/core/color.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//style----------------------------------------
+TextStyle mainTextG = const TextStyle(
+  color: Color.fromARGB(255, 30, 243, 37),
+  fontWeight: FontWeight.w700,
+  fontSize: 35,
+);
+TextStyle shortTextW = const TextStyle(
+  color: Color.fromARGB(255, 254, 254, 254),
+  fontWeight: FontWeight.w700,
+  fontSize: 15,
+);
+TextStyle shortTextB = const TextStyle(
+  color: Color.fromARGB(255, 0, 0, 0),
+  fontWeight: FontWeight.w700,
+  fontSize: 15,
+);
+//color-------------------------------------------
+Color Cgreen = const Color.fromARGB(255, 30, 243, 37);
+Color Cwhite = Colors.white;
+Color Cblack = Colors.black;
