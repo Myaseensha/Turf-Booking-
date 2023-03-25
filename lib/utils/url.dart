e04c880e84e0ae4af5c9c19cbf106287ab004729
yaseensha <shayaseen51@gmail.com>
2023-03-25 11:21:56 +0530

    added new url

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -2,3 +2,4 @@ const String baseUrl = 'http://10.4.3.94:8888';
 const String otp = '$baseUrl/send-otp';
 const String sendotp = '$baseUrl/otp';
 const String resendOtp = '$baseUrl/otp-resend';
+const String ceratcourt = '$baseUrl/turf-registration';
