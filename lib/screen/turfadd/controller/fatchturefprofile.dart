703987dbd2a0b1ff6d66097bdd2dfb2e3e3fb1f5
yaseensha <shayaseen51@gmail.com>
2023-05-02 18:47:38 +0530

    turf fatch

---
+++ lib/screen/turfadd/controller/fatchturefprofile.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:turf/screen/turfadd/model/court_get.dart';
+import 'package:turf/screen/turfadd/utils/url.dart';
+
+Future<CourtDetails?> fetchTurfProfile(
+    String token, BuildContext context) async {
+  print("$token///////////////////////////////////////////////////////");
+  const url = '$baseUrlTurf/turf-profile';
+  try {
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      return CourtDetails.fromJson(responseData);
+    } else {
+      final responseData = jsonDecode(response.body);
+      final message = responseData['message'];
+      final snackBar = SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text('$message'),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      return null;
+    }
+  } catch (error) {
+    print('Error fetching turf profile: $error');
+    return null;
+  }
+}
